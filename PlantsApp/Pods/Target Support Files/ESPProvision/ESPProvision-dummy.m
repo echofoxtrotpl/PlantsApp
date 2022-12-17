daa02384115914f15397435a8d33ca76ae94c953
Sebastian Zdybiowski <66919607+echofoxtrotpl@users.noreply.github.com>
2022-12-17 13:25:24 +0100

    Delete PlantsApp/Pods directory

--- PlantsApp/Pods/Target Support Files/ESPProvision/ESPProvision-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ESPProvision : NSObject
-@end
-@implementation PodsDummy_ESPProvision
-@end
--- PlantsApp/Pods/Target Support Files/Pods-PlantsApp/Pods-PlantsApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlantsApp : NSObject
-@end
-@implementation PodsDummy_Pods_PlantsApp
-@end
